0285f4ff99a3ad3ad6c08ef2e4e8180e09ab8dde
Marian <kohkodan.m@gmail.com>
2025-10-13 14:19:17 +0200

    Added functionality that styores latest operations and option to show this history to user

--- NoNameLudovik.Calculator/CalculatorLibrary/CalculatorLibrary.cs
+++ NoNameLudovik.Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -6,6 +6,7 @@ namespace CalculatorLibrary
     public class Calculator
     {
         JsonWriter writer;
+        List<Calculation> calculations = new List<Calculation>(); //List for history of calculations
 
         public Calculator()
         {
@@ -18,7 +19,6 @@ namespace CalculatorLibrary
             writer.WriteStartArray();
         }
 
-        // CalculatorLibrary.cs
         public double DoOperation(double num1, double num2, string op)
         {
             double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
@@ -34,14 +34,17 @@ namespace CalculatorLibrary
                 case "a":
                     result = num1 + num2;
                     writer.WriteValue("Add");
+                    op = "Add";
                     break;
                 case "s":
                     result = num1 - num2;
                     writer.WriteValue("Subtract");
+                    op = "Subtract";
                     break;
                 case "m":
                     result = num1 * num2;
                     writer.WriteValue("Multiply");
+                    op = "Multiply";
                     break;
                 case "d":
                     // Ask the user to enter a non-zero divisor.
@@ -50,6 +53,7 @@ namespace CalculatorLibrary
                         result = num1 / num2;
                     }
                     writer.WriteValue("Divide");
+                    op = "Divide";
                     break;
                 // Return text for an incorrect option entry.
                 default:
@@ -59,15 +63,61 @@ namespace CalculatorLibrary
             writer.WriteValue(result);
             writer.WriteEndObject();
 
+            calculations.Add(new Calculation(num1, num2, op, result));//Add operation to history
+
             return result;
         }
 
-        // CalculatorLibrary.cs
+        //Method for showing history to user
+        public void ShowHistory() 
+        {
+            Console.Clear();
+            //Check if list is empty. If not than show to user all objects from calculations List
+            if (calculations.Count == 0)
+            {
+                Console.WriteLine("History is empty!)");
+                Console.ReadLine();
+            }
+            else
+            {
+                int index = 1;
+                foreach (Calculation calculation in calculations)
+                {
+                    Console.WriteLine($"{index}. Numbers:{calculation.num1}, {calculation.num2}|{calculation.op}|Result: {calculation.result}");
+                    index++;
+                }
+
+                Console.WriteLine("Type 'd' if you want to delete history or just press enter if you want to leave");
+                if(Console.ReadLine() == "d")
+                {
+                    calculations.Clear();
+                }
+            }
+            Console.Clear();
+        }
+
         public void Finish()
         {
             writer.WriteEndArray();
             writer.WriteEndObject();
             writer.Close();
+        }
+    }
+
+    //Class for determine calculation objects in history
+    public class Calculation
+    {
+        public double num1;
+        public double num2;
+        public string op;
+        public double result;
+
+        public Calculation(double num1, double num2, string op, double result)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.op = op;
+            this.result = result;
         }
     }
 }
--- NoNameLudovik.Calculator/NoNameLudovik.Calculator/Program.cs
+++ NoNameLudovik.Calculator/NoNameLudovik.Calculator/Program.cs
@@ -6,19 +6,47 @@ class Program
     static void Main(string[] args)
     {
         bool endApp = false;
-        // Display title as the C# console calculator app.
-        Console.WriteLine("Console Calculator in C#\r");
-        Console.WriteLine("------------------------\n");
 
         Calculator calculator = new Calculator();
         while (!endApp)
         {
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
             // Declare variables and set to empty.
             // Use Nullable types (with ?) to match type of System.Console.ReadLine
             string? numInput1 = "";
             string? numInput2 = "";
             double result = 0;
+            string? op;
 
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.WriteLine("\th - Show History of Calculation");
+            Console.Write("Your option? ");
+
+            // Validate input is not null, and matches the pattern
+            while (true)
+            {
+                op = Console.ReadLine();
+
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d|h]"))
+                {
+                    Console.Write("Error: Unrecognized input. Try again:");
+                    continue;
+                }
+                break;
+            }
+
+            if (op == "h")
+            {
+                calculator.ShowHistory();
+                continue;
+            }
             // Ask the user to type the first number.
             Console.Write("Type a number, and then press Enter: ");
             numInput1 = Console.ReadLine();
@@ -41,37 +69,20 @@ class Program
                 numInput2 = Console.ReadLine();
             }
 
-            // Ask the user to choose an operator.
-            Console.WriteLine("Choose an operator from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            string? op = Console.ReadLine();
-
-            // Validate input is not null, and matches the pattern
-            if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+            try
             {
-                Console.WriteLine("Error: Unrecognized input.");
+                result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
             }
-            else
+            catch (Exception e)
             {
-                try
-                {
-                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
-                    if (double.IsNaN(result))
-                    {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
-                    }
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
-                }
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
             }
+            
             Console.WriteLine("------------------------\n");
 
             // Wait for the user to respond before closing.
@@ -79,6 +90,7 @@ class Program
             if (Console.ReadLine() == "n") endApp = true;
 
             Console.WriteLine("\n"); // Friendly linespacing.
+            Console.Clear();
         }
         // Add call to close the JSON writer before return
         calculator.Finish();
